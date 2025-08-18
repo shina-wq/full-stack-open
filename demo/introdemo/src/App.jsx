46c223801caf8f5ee54621ace93c1a31d4a0e9cd
Shina <m.atete@alustudent.com>
2025-08-18 13:40:42 +0200

    part 1.a exercises

---
+++ demo/introdemo/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='George' />
+      <Hello name='Daisy' />
+    </div>
+  )
+}
+
+export default App
