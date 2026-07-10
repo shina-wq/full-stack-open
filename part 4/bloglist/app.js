20aeaf3da0236324975fa80311894e53ebbabf9f
Shina Atete Mpeta <atetempeta@gmail.com>
2026-07-10 22:03:15 +0200

    4.1-4.2

---
+++ part 4/bloglist/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const blogsRouter = require("./controllers/blogs");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
